5df8a0ca2cd43c9e7338e4bdf1226809ec61d748
Amogh Dabholkar <amogh2599@gmail.com>
2020-07-24 14:30:04 +0530

    Updated rrt_implement

--- RRT_Discrete-master/rrt_run.m
+++ RRT_Discrete-master/rrt_run.m
@@ -5,5 +5,6 @@ environment = load('environment.mat');
 x = 12;
 source = [1,int32(147/x)];
 goal = [int32(244/x),3];
-[edges,vertices] = rrt_implement(environment.empty_world, source, goal, 1);
+step_size = 3;
+[edges,vertices] = rrt_implement(environment.empty_world, source, goal, step_size);
 %[edges,vertices] = rrt_start_implement(environment.empty_world, source, goal, 3, 3);
